303157f116d8c579cb627ee5372d6e6e5ded087e
Sher Minn Chong <sherminn.chong@gmail.com>
2015-09-30 15:22:05 -0400

    Implement line entry

--- mta_turnstile_scraper.py
+++ mta_turnstile_scraper.py
@@ -15,26 +15,56 @@ import sqlite3
 COLUMN_HEADERS = "CA,UNIT,SCP,STATION,LINENAME,DIVISION,DATE,TIME,DESC,ENTRIES,EXITS".split(',') 
 COLUMN_DATATYPES = "TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,DATE,TIME,TEXT,INTEGER,INTEGER".split(',')
 
+# Database
+DATABASE = 'test.db'
+connection = sqlite3.connect(DATABASE)
+cursor = connection.cursor()
+
 """
 Given a turnstile data file, parse data and dump into database
 """
-def parse_file(file):
+def file_to_db(filename):
+
+    with open(filename) as f:
+        next(f) # skip header
+        for line in f:
+            add_entry_db(line)
+    commit_db()
+    return
+
+"""
+Commit db entries
+"""
+def commit_db():
+    connection.commit()
+    return
+
+"""
+Given a line of entry from raw, add that entry to db without commiting
+"""
+def add_entry_db(line):
+    # format values accordingly
+    values = []
+    count = 0
+    for val in line.split(','):
+        if COLUMN_DATATYPES[count] == 'INTEGER':
+            values.append(val)
+        else:
+            values.append("'" + val + "'")
+
+    insert_query = 'INSERT INTO entries VALUES(' + ",".join(values) + ')'
+    print(insert_query)
+    cursor.execute(insert_query)
     return
 
 """
 Set up database
 """
-def init_db(database):
-    # Queries
+def init_db():
+    # create headers for entries 
     header_and_datatypes = ", ".join([COLUMN_HEADERS[i] + ' ' + COLUMN_DATATYPES[i] for i in range(len(COLUMN_HEADERS))])
+    create_query =  'CREATE TABLE IF NOT EXISTS entries (' + header_and_datatypes + ')'
     
-    print(header_and_datatypes)
-    
-    Q = {
-        'create' : 'CREATE TABLE IF NOT EXISTS entries (' + header_and_datatypes + ')'
-    }
-    print(Q['create'])
-    connection = sqlite3.connect(database)
-    cursor = connection.cursor()
-    cursor.execute(Q['create'])
+    # create 'entries' table
+    cursor.execute(create_query)
     return
