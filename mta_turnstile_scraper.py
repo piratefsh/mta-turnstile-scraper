998332877ef1fcdb259b52c35f5ea350e45f176c
Sher Minn Chong <sherminn.chong@gmail.com>
2015-09-30 14:34:11 -0400

    Create db

--- mta_turnstile_scraper.py
+++ mta_turnstile_scraper.py
@@ -10,6 +10,11 @@ Usage: python mta_turnsite_scraper.py <db>
 
 import sqlite3
 
+# Globals
+# Columns
+COLUMN_HEADERS = "CA,UNIT,SCP,STATION,LINENAME,DIVISION,DATE,TIME,DESC,ENTRIES,EXITS".split(',') 
+COLUMN_DATATYPES = "TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,DATE,TIME,TEXT,INTEGER,INTEGER".split(',')
+
 """
 Given a turnstile data file, parse data and dump into database
 """
@@ -20,7 +25,16 @@ def parse_file(file):
 Set up database
 """
 def init_db(database):
-    connection = sqlite3.connect(db)
+    # Queries
+    header_and_datatypes = ", ".join([COLUMN_HEADERS[i] + ' ' + COLUMN_DATATYPES[i] for i in range(len(COLUMN_HEADERS))])
+    
+    print(header_and_datatypes)
+    
+    Q = {
+        'create' : 'CREATE TABLE entries (' + header_and_datatypes + ')'
+    }
+    print(Q['create'])
+    connection = sqlite3.connect(database)
     cursor = connection.cursor()
-
+    cursor.execute(Q['create'])
     return
